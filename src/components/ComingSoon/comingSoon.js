6c1466e447f1200edac782acba6097af1d4862a2
saad-68 <95625780+Saad-76@users.noreply.github.com>
2022-02-03 02:03:48 -0800

    First Commit Data

---
+++ src/components/ComingSoon/comingSoon.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./comingSoon"
+
+const ComingSoon=()=> {
+  return (
+      <>
+<div className="row">
+    <div className="col-md-6">
+<h2>Coming Soon...</h2>
+    </div>
+    <div className="col-md-6">
+
+    </div>
+
+</div>
+      </>
+  );
+}
+
+export default ComingSoon;
